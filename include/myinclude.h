9edaa4cc07dd0d671e80f1a9a7bbc50d4bd1d120
i82sagaf <i82sagaf@uco.es>
2020-06-03 03:18:31 +0200

    Hello world in the master

--- include/myinclude.h
+++ include/myinclude.h
@@ -1,7 +1,7 @@
 #include <stdio.h>
 void f()
 {
-	char c1[100]= "Hello world";
+	char c1[100]= "Hello world in the master";
 	char c2[100]= ", I am <your name here>";
 	printf("%s\n", strcat(c1, c2));
 	return 0;
